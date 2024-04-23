fdf52a2bdbe710dc13be1dba0d9194cb700e8591
Siddhant Karki <siddhantskarki@gmail.com>
2024-04-23 17:28:57 +1000

    Login and Registration UI

--- app/src/main/java/com/example/onepunchmessenger/Login.java
+++ app/src/main/java/com/example/onepunchmessenger/Login.java
@@ -1,6 +1,5 @@
 package com.example.onepunchmessenger;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,43 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-public class MainActivity extends AppCompatActivity {
-    //Auth
-    FirebaseAuth auth;
-
-
-
-
-
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        //auth
-        auth=FirebaseAuth.getInstance();
-        if(auth.getCurrentUser()==null){
-            Intent intent=new Intent(MainActivity.this, Registration.class);
-            startActivity(intent);
-            finish();
-
-        }
-
-
-
-//test the FB database
-//        // Write a message to the database
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference myRef = database.getReference("message");
-//
-//        myRef.setValue("Hello, ello!");
     }
 }
